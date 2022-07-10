81bd54f57a8e0ac3e58d191f9d3d5e33295655f3
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-07-10 19:10:10 +0530

    Update approve.py

--- Shikimori/modules/approve.py
+++ Shikimori/modules/approve.py
@@ -212,18 +212,17 @@ dispatcher.add_handler(UNAPPROVEALL_BTN)
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
 
-__mod_name__ = "Approvals ✅"
+__mod_name__ = "Aᴘᴘʀᴏᴠᴇʟs"
 
 __help__ = """
 Sometimes, you might trust a user not to send unwanted content.
 Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
-
-That's what approvals are for - approve of trustworthy users to allow them to send
+That's what approvals are for - approve of trustworthy users to allow them to send 
 
 *Admin commands:*
-❂ `/approval`*:* Check a user's approval status in this chat.
-❂ `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
-❂ `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
-❂ `/approved`*:* List all approved users.
-❂ `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
+✪︎ `/approval`*:* Check a user's approval status in this chat.
+✪︎ `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+✪︎ `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+✪︎ `/approved`*:* List all approved users.
+✪︎ `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
 """
