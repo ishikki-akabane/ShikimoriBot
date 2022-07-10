a7901497937f56902fb0dbe74e56ec5394c52770
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-07-10 19:12:50 +0530

    Update backups.py

--- Shikimori/modules/backups.py
+++ Shikimori/modules/backups.py
@@ -379,7 +379,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups"
+__mod_name__ = "Bᴀᴄᴋᴜᴘs"
 
 IMPORT_HANDLER = CommandHandler("import", import_data, run_async=True)
 EXPORT_HANDLER = CommandHandler(
@@ -391,8 +391,8 @@ dispatcher.add_handler(EXPORT_HANDLER)
 
 
 __help__ = """
-*Backup and restore chat backup*
-
- ❍ `/export` - Sends backup file of the chat.
- ❍ `/import` - Reply to a backup file to restore the chat.
+*Only for group owner:*
+ ❍ /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ Note that files / photos cannot be imported due to telegram restrictions.
+ ❍ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 """
