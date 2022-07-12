ecb5f64101042f2847e99de7bcffc73867d8d0f7
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-07-12 18:41:35 +0530

    Update groupgardian.py

--- Shikimori/modules/groupgardian.py
+++ Shikimori/modules/groupgardian.py
@@ -11,7 +11,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
+"""
 import asyncio
 import os
 import re
@@ -435,3 +435,4 @@ __help__ = """
 """
 __mod_name__ = "Group Guardian"
  
+"""
