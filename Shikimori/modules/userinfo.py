efc51432b58de3cfd3b91b3622b82602f696c563
KACxCHAN <110216345+KACxCHAN@users.noreply.github.com>
2022-07-30 08:20:42 +0530

    Update userinfo.py

--- Shikimori/modules/userinfo.py
+++ Shikimori/modules/userinfo.py
@@ -273,7 +273,7 @@ def info(update: Update, context: CallbackContext):
     else:
         return
 
-    rep = message.reply_text("<code>Appraising...</code>", parse_mode=ParseMode.HTML)
+    rep = message.reply_text("<code>Yum! Yum! Yum...</code>", parse_mode=ParseMode.HTML)
 
     text = (
         f"╒═══「<b>• Appraisal results •</b> 」\n"
